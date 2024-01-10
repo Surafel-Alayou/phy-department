11751072d814e6312c7ef19d216256aefff20972
Surafel-Alayou <surafel2alayou@gmail.com>
2024-01-10 14:59:08 +0300

    Seminar

--- assets/js/seminars.js
+++ assets/js/seminars.js
@@ -5,5 +5,14 @@ function seminar_2(){
     document.getElementById("myframefriday").src = "seminar/2-week.html";
 }
 function seminar_3(){
+    document.getElementById("myframefriday").src = "seminar/3-week.html";
+}
+function seminar_4(){
     document.getElementById("myframefriday").src = "seminar/4-week.html";
+}
+function seminar_5(){
+    document.getElementById("myframefriday").src = "seminar/5-week.html";
+}
+function seminar_6(){
+    document.getElementById("myframefriday").src = "seminar/6-week.html";
 }
