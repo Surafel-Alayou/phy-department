f8cee847529b2d5cadbf7cb9edc6ff66c9c4dd99
Surafel-Alayou <surafel2alayou@gmail.com>
2024-01-29 11:26:05 +0300

    Major changes

---
+++ assets/js/news.js
@@ -0,0 +1,12 @@
+function news_1(){
+    document.getElementById("title").innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit.";
+    document.getElementById("post-img-main").src = "assets/images/news/blog-2.jpg";
+    document.getElementById("post-img-sub").src = "assets/images/news/blog-recent-1.jpg";
+    document.getElementById("quote-para-three").innerHTML = "Voluptatem atque optio, sequi, dignissimos excepturi tenetur ullam vitae dolores laborum aspernatur laboriosam id error explicabo accusantium illum officia dolorem accusamus nostrum?";
+    document.getElementById("para-one").innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem atque optio, sequi, dignissimos excepturi tenetur ullam vitae dolores laborum aspernatur laboriosam id error explicabo accusantium illum officia dolorem accusamus nostrum?";
+    document.getElementById("para-two").innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem atque optio, sequi, dignissimos excepturi tenetur ullam vitae dolores laborum aspernatur laboriosam id error explicabo accusantium illum officia dolorem accusamus nostrum?";
+    document.getElementById("para-four").innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem atque optio, sequi, dignissimos excepturi tenetur ullam vitae dolores laborum aspernatur laboriosam id error explicabo accusantium illum officia dolorem accusamus nostrum?";
+    document.getElementById("para-five").innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem atque optio, sequi, dignissimos excepturi tenetur ullam vitae dolores laborum aspernatur laboriosam id error explicabo accusantium illum officia dolorem accusamus nostrum?";
+    document.getElementById("para-six").innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem atque optio, sequi, dignissimos excepturi tenetur ullam vitae dolores laborum aspernatur laboriosam id error explicabo accusantium illum officia dolorem accusamus nostrum?";
+    document.getElementById("para-seven").innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem atque optio, sequi, dignissimos excepturi tenetur ullam vitae dolores laborum aspernatur laboriosam id error explicabo accusantium illum officia dolorem accusamus nostrum?";
+}
