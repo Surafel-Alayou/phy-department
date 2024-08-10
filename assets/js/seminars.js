599819b9b93f716ef8e5e720db54bb0738fa6d8e
Ghion-Tech-Group <159915175+Ghion-Tech-Group@users.noreply.github.com>
2024-08-10 20:32:37 +0300

    Minor updates

--- assets/js/seminars.js
+++ assets/js/seminars.js
@@ -1,18 +1,24 @@
 function seminar_1(){
     document.getElementById("myframefriday").src = "seminar/1-week.html";
+    document.getElementById("linktoattend").style.display = "none";
 }
 function seminar_2(){
     document.getElementById("myframefriday").src = "seminar/2-week.html";
+    document.getElementById("linktoattend").style.display = "none";
 }
 function seminar_3(){
     document.getElementById("myframefriday").src = "seminar/3-week.html";
+    document.getElementById("linktoattend").style.display = "none";
 }
 function seminar_4(){
     document.getElementById("myframefriday").src = "seminar/4-week.html";
+    document.getElementById("linktoattend").style.display = "none";
 }
 function seminar_5(){
     document.getElementById("myframefriday").src = "seminar/5-week.html";
+    document.getElementById("linktoattend").style.display = "none";
 }
 function seminar_6(){
     document.getElementById("myframefriday").src = "seminar/6-week.html";
+    document.getElementById("linktoattend").style.display = "none";
 }
