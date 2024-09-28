cd29b5196126ba178068343cfa1ceb9ef744d67d
Ghion-Tech-Group <159915175+Ghion-Tech-Group@users.noreply.github.com>
2024-09-28 11:19:41 +0300

    Pages added and contents updated

--- assets/js/news.js
+++ assets/js/news.js
@@ -1,5 +1,5 @@
 function news_1(){
-    document.getElementById("title").innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit.";
+    document.getElementById("title").innerHTML = "Bahir Dar University Graduation Ceremony";
     document.getElementById("post-img-main").src = "assets/images/news/blog-2.jpg";
     document.getElementById("post-img-sub").src = "assets/images/news/blog-recent-1.jpg";
     document.getElementById("quote-para-three").innerHTML = "Voluptatem atque optio, sequi, dignissimos excepturi tenetur ullam vitae dolores laborum aspernatur laboriosam id error explicabo accusantium illum officia dolorem accusamus nostrum?";
@@ -9,4 +9,6 @@ function news_1(){
     document.getElementById("para-five").innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem atque optio, sequi, dignissimos excepturi tenetur ullam vitae dolores laborum aspernatur laboriosam id error explicabo accusantium illum officia dolorem accusamus nostrum?";
     document.getElementById("para-six").innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem atque optio, sequi, dignissimos excepturi tenetur ullam vitae dolores laborum aspernatur laboriosam id error explicabo accusantium illum officia dolorem accusamus nostrum?";
     document.getElementById("para-seven").innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem atque optio, sequi, dignissimos excepturi tenetur ullam vitae dolores laborum aspernatur laboriosam id error explicabo accusantium illum officia dolorem accusamus nostrum?";
+    document.getElementById("recent-1-img").src = "assets/images/news/blog-1.jpg";
+    document.getElementById("recent-1-title").innerHTML = "President Visit to Science College";
 }
